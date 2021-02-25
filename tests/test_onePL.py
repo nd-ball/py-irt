613f315c5f44dff16582cc2b497c4bd9524d8d9a
John P. Lalor <john.lalor@github.com>
2021-02-25 12:50:49 -0500

    setting up travisci

---
+++ tests/test_onePL.py
@@ -0,0 +1,9 @@
+
+
+from py_irt.models.one_param_logistic import OneParamLog
+
+
+#m = OneParamLog()
+
+def test_onePL():
+    assert True == True
---
+++ tests/test_twoPL.py
@@ -0,0 +1,9 @@
+
+
+from py_irt.models.two_param_logistic import TwoParamLog
+
+
+#m = TwoParamLog()
+
+def test_twoPL():
+    assert True == True
