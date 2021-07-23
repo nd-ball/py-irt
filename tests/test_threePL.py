ba66f47b25086819e3beb81b49f87edad41a5f03
John Lalor <john.lalor@nd.edu>
2021-07-23 07:23:29 -0400

    a few small tweaks to line up 3pl, I still need to refactor to be consistent with new changes

--- tests/test_threePL.py
+++ tests/test_threePL.py
@@ -1,4 +1,5 @@
 # preliminaries
+from py_irt.config import IrtConfig
 import unittest
 
 # import model for testing
@@ -8,22 +9,23 @@ from py_irt.training import IrtModelTrainer
 
 class TestThreePL(unittest.TestCase):
     def test_training(self):
-        trainer = IrtModelTrainer(model_type="3pl", data_path="test_fixtures/minitest.jsonlines")
-        trainer.train(iterations=100, device="cpu")
+        config = IrtConfig(model_type="3pl", epochs=100)
+        trainer = IrtModelTrainer(config=config, data_path="test_fixtures/minitest.jsonlines")
+        trainer.train(device="cpu")
         trainer.save("/tmp/parameters.json")
 
     def test_priors(self):
         with self.assertRaises(NotImplementedError):
-            m = FourParamLog("testing", "cpu", 100, 100, False)
+            m = ThreeParamLog("testing", "cpu", 100, 100, False)
 
     def test_device(self):
         with self.assertRaises(ValueError):
-            m = FourParamLog("hierarchical", "zpu", 100, 100, False)
+            m = ThreeParamLog("hierarchical", "zpu", 100, 100, False)
 
     def test_num_items(self):
         with self.assertRaises(ValueError):
-            m = FourParamLog("hierarchical", "cpu", -100, 100, False)
+            m = ThreeParamLog("hierarchical", "cpu", -100, 100, False)
 
     def test_num_subjects(self):
         with self.assertRaises(ValueError):
-            m = FourParamLog("hierarchical", "cpu", 100, -100, False)
+            m = ThreeParamLog("hierarchical", "cpu", 100, -100, False)
