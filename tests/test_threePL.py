7b021bb4170a6499a9273dde502792e0de7f6044
John Lalor <john.lalor@nd.edu>
2022-01-26 14:44:29 -0500

    get rid of the irt_models dict now that we have registry

--- tests/test_threePL.py
+++ tests/test_threePL.py
@@ -9,7 +9,7 @@ from py_irt.training import IrtModelTrainer
 
 class TestThreePL(unittest.TestCase):
     def test_training(self):
-        config = IrtConfig(model_type="3pl", epochs=100)
+        config = IrtConfig(model_type="3pl", epochs=100, priors="hierarchical")
         trainer = IrtModelTrainer(config=config, data_path="test_fixtures/minitest.jsonlines")
         trainer.train(device="cpu")
         trainer.save("/tmp/parameters.json")
