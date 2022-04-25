6e0663986db72bb7aebeb574c8f6ba1920d4ed27
John Lalor <john.lalor@nd.edu>
2022-04-25 13:54:33 -0400

    bugfix in scoring module

--- py_irt/scoring.py
+++ py_irt/scoring.py
@@ -8,17 +8,18 @@ from scipy.stats import lognorm, norm
 from scipy.special import expit
 
 
-def theta_fn(difficulties, student_prior, response_pattern):
+def theta_fn(difficulties, response_pattern):
     """Estimate theta for a given response pattern"""
 
     def fn(theta):
         theta = theta[0]
         probabilities = expit(theta - difficulties)
-        # print(probabilities)
-        log_likelihood = student_prior.logpdf(theta)
+        log_likelihood = 0
         for i, rp in enumerate(response_pattern):
-            log_likelihood += np.log1p((2 * probabilities[i] - 1) * rp)
-        # print(log_likelihood)
+            if rp == 0:
+                log_likelihood += np.log(1-probabilities[i])
+            else:
+                log_likelihood += np.log(probabilities[i])
         return -log_likelihood
 
     return fn
@@ -30,14 +31,13 @@ def calculate_theta(difficulties, response_pattern, num_obs=-1):
     if num_obs > 0, then sample from the observed values for a computational speedup
     """
 
-    student_prior = norm(loc=0.0, scale=1.0)
     if num_obs > 0:
         samples = np.random.choice(len(difficulties), num_obs)
         difficulties = [difficulties[s] for s in samples]
         response_pattern = [response_pattern[s] for s in samples]
 
-    fn = theta_fn(difficulties, student_prior, response_pattern)
-    result = minimize(fn, [0.1], method="Nelder-Mead")
+    fn = theta_fn(difficulties, response_pattern)
+    result = minimize(fn, [0.1], method="BFGS")
     return result["x"]
 
 
