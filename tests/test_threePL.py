fbadc309d47839335266219fc32da89888d11adb
charchit7 <charchitsharma11@gmail.com>
2022-05-01 03:20:11 +0530

    priors test not needed!

--- tests/test_threePL.py
+++ tests/test_threePL.py
@@ -10,15 +10,10 @@ from py_irt.training import IrtModelTrainer
 class TestThreePL(unittest.TestCase):
     def test_training(self):
         config = IrtConfig(model_type="3pl", epochs=100, priors="hierarchical")
-        trainer = IrtModelTrainer(config=config, data_path="test_fixtures/minitest.jsonlines")
+        trainer = IrtModelTrainer(
+            config=config, data_path="test_fixtures/minitest.jsonlines")
         trainer.train(device="cpu")
         trainer.save("/tmp/parameters.json")
-
-    def test_priors(self):
-        with self.assertRaises(ValueError):
-            m = ThreeParamLog(
-                priors="testing", device="cpu", num_items=100, num_subjects=100, verbose=False
-            )
 
     def test_device(self):
         with self.assertRaises(ValueError):
